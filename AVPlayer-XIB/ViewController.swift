5f4c74064ab75b2f6ee4d8cc19febbb1e0b39005
Seungpyo Hong <rush94434@gmail.com>
2025-11-05 17:16:29 +0900

    Initial Commit

---
+++ AVPlayer-XIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVPlayer-XIB
+//
+//  Created by 홍승표 on 11/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
