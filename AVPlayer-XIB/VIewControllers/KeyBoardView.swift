a08d1fa1453cde5a012e8b5d76229e0cdf42fb6a
Seungpyo Hong <rush94434@gmail.com>
2025-11-20 17:41:48 +0900

    feat: XIB UI 제약 조건 수정

---
+++ AVPlayer-XIB/VIewControllers/KeyBoardView.swift
@@ -0,0 +1,8 @@
+//
+//  KeyBoardView.swift
+//  AVPlayer-XIB
+//
+//  Created by 홍승표 on 11/19/25.
+//
+
+import Foundation
---
+++ AVPlayer-XIB/VIewControllers/KeyPadView.swift
@@ -0,0 +1,20 @@
+//
+//  KeyPadVIew.swift
+//  AVPlayer-XIB
+//
+//  Created by 홍승표 on 11/19/25.
+//
+
+import UIKit
+
+class KeyPadVIew: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
