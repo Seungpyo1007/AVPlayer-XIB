7e60bffaffddb2b64653b7cf5f7690301898e9a7
Seungpyo Hong <rush94434@gmail.com>
2025-11-21 10:27:24 +0900

    feat: KeyBoardView and KeyPadView 추가

--- AVPlayer-XIB/VIewControllers/KeyBoardView.swift
+++ AVPlayer-XIB/VIewControllers/KeyBoardView.swift
@@ -5,4 +5,35 @@
 //  Created by 홍승표 on 11/19/25.
 //
 
-import Foundation
+
+import UIKit
+
+class KeyBoardView: UIView {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var tenButton: UIButton!
+    @IBOutlet weak var elevenButton: UIButton!
+    @IBOutlet weak var twelveButton: UIButton!
+    
+    private var digitButtons: [UIButton] {
+        [oneButton, twoButton, threeButton,
+         fourButton, fiveButton, sixButton,
+         sevenButton, eightButton, nineButton,
+         tenButton, elevenButton].compactMap { $0 }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+
+
--- AVPlayer-XIB/VIewControllers/KeyPadView.swift
+++ AVPlayer-XIB/VIewControllers/KeyPadView.swift
@@ -1,5 +1,5 @@
 //
-//  KeyPadVIew.swift
+//  KeyPadView.swift
 //  AVPlayer-XIB
 //
 //  Created by 홍승표 on 11/19/25.
@@ -7,14 +7,32 @@
 
 import UIKit
 
-class KeyPadVIew: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class KeyPadView: UIView {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var tenButton: UIButton!
+    @IBOutlet weak var elevenButton: UIButton!
+    @IBOutlet weak var twelveButton: UIButton!
+    
+    private var digitButtons: [UIButton] {
+        [oneButton, twoButton, threeButton,
+         fourButton, fiveButton, sixButton,
+         sevenButton, eightButton, nineButton,
+         tenButton, elevenButton].compactMap { $0 }
     }
-    */
-
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
 }
+
